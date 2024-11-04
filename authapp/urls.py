ca41ed81740109a2adbc19338bcaa36f7d358fa9
Hassan Ed-Dyb <hed-dyb@e2r4p3.1337.ma>
2024-11-04 18:30:06 +0100

    save

---
+++ authapp/urls.py
@@ -0,0 +1,10 @@
+# authapp/urls.py
+from django.urls import path, include
+
+from django.urls import path
+from .views import RegisterView, LoginView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+]
